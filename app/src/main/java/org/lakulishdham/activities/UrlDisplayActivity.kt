f7a2ce008c2b03c4a08057ade8baa44cb7336c06
mihir.parkhe <mihirpark.parkhe@gmail.com>
2023-10-29 11:10:15 +0530

    minor UI changes for html page

--- app/src/main/java/org/lakulishdham/activities/UrlDisplayActivity.kt
+++ app/src/main/java/org/lakulishdham/activities/UrlDisplayActivity.kt
@@ -2,8 +2,10 @@ package org.lakulishdham.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_url_display.wv_url
 import org.lakulishdham.R
+import org.lakulishdham.helper.closeScreen
 
 class UrlDisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,9 @@ class UrlDisplayActivity : AppCompatActivity() {
         wv_url.settings.javaScriptEnabled = true
         wv_url.settings.allowFileAccess = true
         wv_url.loadUrl("file:///android_asset/payment_button.html");
+    }
+
+    fun goBack(view: View) {
+        closeScreen()
     }
 }
